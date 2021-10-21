b28ae30ddf6eeacd49ed3d5e12f36648debe76ee
Dior1726 <muxammad_909@mail.ru>
2021-10-21 23:26:23 +0600

    adding dist files

---
+++ js/chunk-2d0d7111.10e67450.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d7111"],{"74db":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},r=[],u={},o=u,s=t("2877"),a=Object(s["a"])(o,c,r,!1,null,"54291d6c",null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0d7111.10e67450.js.map
